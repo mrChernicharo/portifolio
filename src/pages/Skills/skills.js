65d43e08e5b39a7d4a81843780ac86db111b9ebf
mrChernicharo <felipe.chernicharo@gmail.com>
2020-05-21 22:49:17 -0300

    minor changes

--- src/pages/Skills/skills.js
+++ src/pages/Skills/skills.js
@@ -2,24 +2,26 @@ import logo from '../../assets/icons/javascript.svg'
 import logo1 from '../../assets/icons/typescript.svg'
 import logo2 from '../../assets/icons/python.svg'
 import logo3 from '../../assets/icons/php.svg'
-import logo4 from '../../assets/icons/database.svg'
+import logo4 from '../../assets/icons/node.svg'
 import logo5 from '../../assets/icons/html5.svg'
-import logo6 from '../../assets/icons/css3.svg'
-import logo7 from '../../assets/icons/javascript.svg'
-import logo8 from '../../assets/icons/javascript.svg'
-import logo9 from '../../assets/icons/javascript.svg'
-import logo10 from '../../assets/icons/react.svg'
-import logo11 from '../../assets/icons/react-native.svg'
+import logo6 from '../../assets/icons/css.svg'
+import logo7 from '../../assets/icons/react.svg'
+import logo8 from '../../assets/icons/react-native.svg'
+import logo9 from '../../assets/icons/react-native.svg'
+
+import logo10 from '../../assets/icons/apple.svg'
+import logo11 from '../../assets/icons/vscode.png'
 import logo12 from '../../assets/icons/postgres.svg'
 import logo13 from '../../assets/icons/mysql.svg'
 import logo14 from '../../assets/icons/mongodb.svg'
-import logo15 from '../../assets/icons/node.svg'
+import logo15 from '../../assets/icons/database.svg'
 import logo16 from '../../assets/icons/node.svg'
-import logo17 from '../../assets/icons/javascript.svg'
+import logo17 from '../../assets/icons/docker.svg'
 import logo18 from '../../assets/icons/javascript.svg'
 import logo19 from '../../assets/icons/javascript.svg'
-import logo20 from '../../assets/icons/docker.svg'
-import logo21 from '../../assets/icons/figma.svg'
+
+import logo20 from '../../assets/icons/figma.svg'
+import logo21 from '../../assets/icons/dbeaver.svg'
 import logo22 from '../../assets/icons/insomnia.svg'
 import logo23 from '../../assets/icons/wamp.svg'
 import logo24 from '../../assets/icons/mamp.png'
@@ -45,8 +47,8 @@ export const skills1 = [
   {name:'SQL', img: `${images[4]}`},
   {name:'Html', img: `${images[5]}`},
   {name:'Css', img: `${images[6]}`},
-  // {name:'', img: `${images[7]}`},
-  // {name:'', img: `${images[8]}`},
+  {name:'', img: `${images[7]}`},
+  {name:'', img: `${images[8]}`},
   // {name:'', img: `${images[9]}`},
 ]
 export const skills2 = [
@@ -56,20 +58,20 @@ export const skills2 = [
   {name:'MySQL', img: `${images[13]}`},
   {name:'MongoDB', img: `${images[14]}`},
   {name:'Node', img: `${images[15]}`},
-  // {name:'', img: `${images[16]}`},
-  // {name:'', img: `${images[17]}`},
+  {name:'', img: `${images[16]}`},
+  {name:'', img: `${images[17]}`},
   // {name:'', img: `${images[18]}`},
   // {name:'', img: `${images[19]}`},
 ]
 export const skills3 = [
-  {name:'Docker', img: `${images[20]}` },
+  {name:'Figma', img: `${images[20]}` },
   {name:'Figma', img: `${images[21]}`},
   {name:'Insomnia', img: `${images[22]}`},
   {name:'Wamp', img: `${images[23]}`},
   {name:'Mamp', img: `${images[24]}`},
   {name:'Bootstrap', img: `${images[25]}`},
   {name:'Notion', img: `${images[26]}`},
-  {name:'DBeaver', img: `${images[27]}`},
+  // {name:'DBeaver', img: `${images[27]}`},
   // {name:'', img: `${images[28]}`},
   // {name:'', img: `${images[29]}`},
 ]
